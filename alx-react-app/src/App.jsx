8a58c431e0c054de993f749387dc15da2a4fb3cd
Miftah <miftahaadem@gmail.com>
2024-08-07 10:44:18 +0300

    Added  UserProfile.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,11 +6,13 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <UserProfile name="Miftah" bio="Loves hiking and photography" age={30} />
       <Header />
       <MainContent />
       <Footer />
