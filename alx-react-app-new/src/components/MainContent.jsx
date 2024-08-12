19e39d550eb10281e2908162c20fe6a0f8c6824c
Miftah <miftahaadem@gmail.com>
2024-08-12 09:01:45 +0300

    initial commit

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent() {
+  return (
+    <main style={{ backgroundColor: 'blueviolet' }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  )
+}
+export default MainContent
