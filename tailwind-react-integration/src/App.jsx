be65ac0f9f8b249069ffd231bc394441a3d8a3a6
Miftah <miftahaadem@gmail.com>
2024-09-02 11:52:06 +0300

    intial commit

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,5 +1,12 @@
+import UserProfile from './component/UserProfile'
+
 function App() {
-  return <div className="bg-gray-100">This is a React app with tailwind</div>
+  return (
+    <>
+      {/* <div className="bg-gray-100">This is a React app with tailwind</div> */}
+      <UserProfile />
+    </>
+  )
 }
 
 export default App
