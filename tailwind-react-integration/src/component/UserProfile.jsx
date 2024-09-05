6f10e680fcf1e4b1ed8184f97e66049444d26b56
Miftah <miftahaadem@gmail.com>
2024-09-05 12:54:18 +0300

    initial commit

--- tailwind-react-integration/src/component/UserProfile.jsx
+++ tailwind-react-integration/src/component/UserProfile.jsx
@@ -1,8 +1,6 @@
-import React from 'react'
-
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
